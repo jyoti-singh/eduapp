bae81446e1d6d0e77ab52fe52d5c947312b65473
root <root@piyush-Inspiron-620s.(none)>
2013-04-03 17:29:27 +0530

    commit data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401050126) do
+ActiveRecord::Schema.define(:version => 20130403115231) do
 
   create_table "educations", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20130401050126) do
     t.float    "marks_in_12th"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.text     "comment"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
